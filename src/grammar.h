64927aeaece42ec8736fabdb0af3dcef08bbaee8
kale <42823429+oelin@users.noreply.github.com>
2023-03-23 19:10:05 +0000

    Update grammar.h

--- src/grammar.h
+++ src/grammar.h
@@ -22,14 +22,7 @@ Grammar *grammarConstruct(GrammarType, Grammar *, Grammar *);
 void grammarDeconstruct(Grammar *);
 
 
-#define Proposition(l) grammarConstruct( \
-        GrammarTypeProposition, (Grammar *) l, NULL)
-
-#define Conjunction(l, r) grammarConstruct( \
-        GrammarTypeConjunction, l, r)
-
-#define Disjunction(l, r) grammarConstruct( \
-        GrammarTypeDisjunction, l, r)
-
-#define Implication(l, r) grammarConstruct( \
-        GrammarTypeImplication, l, r)
+#define Proposition(l) grammarConstruct(GrammarTypeProposition, (Grammar *) l, NULL)
+#define Conjunction(l, r) grammarConstruct(GrammarTypeConjunction, l, r)
+#define Disjunction(l, r) grammarConstruct(GrammarTypeDisjunction, l, r)
+#define Implication(l, r) grammarConstruct(GrammarTypeImplication, l, r)
