ac087de1303bef6d5439d5482de54183feece04e
kale <42823429+oelin@users.noreply.github.com>
2023-03-23 19:05:33 +0000

    Create grammar.h

---
+++ src/grammar.h
@@ -0,0 +1,35 @@
+#pragma once
+
+
+typedef enum {
+        GrammarTypeProposition,
+        GrammarTypeConjunction,
+        GrammarTypeDisjunction,
+        GrammarTypeImplication
+} GrammarType;
+
+
+typedef struct Grammar Grammar;
+
+struct Grammar {
+        Grammar *left;
+        Grammar *right;
+        GrammarType type;
+};
+
+
+Grammar *grammarConstruct(GrammarType, Grammar *, Grammar *);
+void grammarDeconstruct(Grammar *);
+
+
+#define Proposition(l) grammarConstruct( \
+        GrammarTypeProposition, (Grammar *) l, NULL)
+
+#define Conjunction(l, r) grammarConstruct( \
+        GrammarTypeConjunction, l, r)
+
+#define Disjunction(l, r) grammarConstruct( \
+        GrammarTypeDisjunction, l, r)
+
+#define Implication(l, r) grammarConstruct( \
+        GrammarTypeImplication, l, r)
