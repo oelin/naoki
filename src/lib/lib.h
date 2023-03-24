f95e489ac8de4385cf3256a2fbfe3e0195d4a416
kale <42823429+oelin@users.noreply.github.com>
2023-03-24 11:45:22 +0000

    Update lib.h

--- src/lib/lib.h
+++ src/lib/lib.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdlib.h>
 
 
 typedef enum {
@@ -35,3 +36,24 @@ void treeDeconstruct(Tree *);
 
 #define Implication(l, r) treeConstruct( \
         TreeTypeImplication, l, r)
+
+
+Tree *treeConstruct(
+        TreeType type,
+        Tree *left,
+        Tree *right
+) {
+        Tree *tree = (Tree *) calloc(1, sizeof(Tree));
+
+        tree->type = type;
+        tree->left = left;
+        tree->right = right;
+
+        return tree;
+}
+
+
+void treeDeconstruct(Tree *tree) {
+        if (tree->left) treeDeconstruct(tree->left);
+        if (tree->right) treeDeconstruct(tree->right);
+}
